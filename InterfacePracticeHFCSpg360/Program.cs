baf1a5fca17d0c1e2010f5e1ce430bc483ab3aae
Sean McKinney <seanamckinney8@gmail.com>
2022-05-09 11:24:11 -0500

    initial commit.  Created a class, created an interface, implemented interface and satisfied contract to use interface.

--- InterfacePracticeHFCSpg360/Program.cs
+++ InterfacePracticeHFCSpg360/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace InterfacePracticeHFCSpg360
 {
+
+    class TallGuy : IClown
+    {
+        public string Name;
+        public int Height;
+
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine($"My name is {Name} and I'm {Height} inches tall.");          
+        }//end TAY()
+
+        public void Honk()
+        {
+            Console.WriteLine($"Honk! Honk!");
+        }//end Honk()
+
+        public string FunnyThingIHave
+        {
+            get { return "Oversized Pants"; }
+        }
+
+    }//end calss
+
     class Program
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+            TallGuy tallGuy = new TallGuy() { Height = 76, Name = "Jimmy" };
+            tallGuy.TalkAboutYourself();
+            Console.WriteLine($"The tall guy has {tallGuy.FunnyThingIHave}");
+            tallGuy.Honk();
+        }//end main
+    }//end class
+}//end namespace
